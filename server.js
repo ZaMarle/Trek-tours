de0951e327c04733264f2720c5ecddabdfa8e705
Zachary <qu4ckr@gmail.com>
2020-01-28 09:38:09 +1000

    changed server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
-});
-
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("App listening on port")
+);
